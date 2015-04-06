00af6fa147bdf65d4ecacd6012890e16fada7fb8
András Veres-Szentkirályi <vsza@vsza.hu>
2015-04-06 21:25:43 +0200

    remove WSGI support for now

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ TEMPFILE=/tmp/lab5soa$$.zip
 wget https://github.com/adatlabor/soa-demo/archive/master.zip -O $TEMPFILE
 unzip $TEMPFILE
 mv soa-demo-master soa
-chmod 701 soa
+chmod 700 soa
 rm $TEMPFILE
