2655c3cc3193fe9d2c1bf2f26a6df0f1e682c772
András Veres-Szentkirályi <vsza@vsza.hu>
2016-04-03 23:48:58 +0200

    added insecure mode to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 TEMPFILE=/tmp/lab5soa$$.zip
-wget https://github.com/adatlabor/soa-demo/archive/master.zip -O $TEMPFILE
+if [ $INSECURE ]; then
+	echo '/!\ Using insecure mode, be careful what you wish for!'
+	WGET_OPTS=--no-check-certificate
+fi
+wget https://github.com/adatlabor/soa-demo/archive/master.zip -O $TEMPFILE $WGET_OPTS
 unzip $TEMPFILE
 mv soa-demo-master soa
 chmod 700 soa
