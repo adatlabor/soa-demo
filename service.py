5634b4cc2b329e02338b35641061ab7ae8287778
András Veres-Szentkirályi <vsza@vsza.hu>
2017-03-31 16:43:36 +0200

    Pylint: handle ancient versions of Pylint

--- service.py
+++ service.py
@@ -150,7 +150,7 @@ def get_db():
 
 
 if __name__ == "__main__":
-    # pylint: disable=wrong-import-position,wrong-import-order
+    # pylint: disable=bad-option-value,wrong-import-position,wrong-import-order
     import os
     os.environ['NLS_LANG'] = '.UTF8'
     app.run(debug=True, port=os.getuid() + 10000)
